aaf83f0de310901418a68a0dee8a60ff9966a7e2
ottenokLeshi <ottenok.belogo@gmail.com>
2018-04-22 00:50:50 +0300

    map is changeble

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import createLine from './lineControllers';
 import lines from './lines';
 
-lines.forEach(line => {
-  createLine(line[0], line[1], 'blue')
-  console.log(line)
+lines.forEach((line, index) => {
+  createLine(line[0], line[1], 'blue');
 });
+
+//setTimeout(startGame(20000), 1000);
+
